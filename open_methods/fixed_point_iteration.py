0bebd0296079f8151287863184e61c78bac082d9
Aureeeus <johndavidlibrado@gmail.com>
2026-01-24 12:18:37 +0800

    Added borderline before and after table for clean illustration. Files are now exclusively a utility.

--- open_methods/fixed_point_iteration.py
+++ open_methods/fixed_point_iteration.py
@@ -1,12 +1,9 @@
-def g(x):
-  return ((2*x + 5) / 2)**(1/3)
-
-
 def fixed_point(g, x0, tol=0.001):
   x_list = [x0]
-  iteration = 2
+  iteration = 1
 
   print(f" Iteration | {'x_n':^9s} | {'x_n+1':^9s} | {'Difference'}")
+  print("-" * 50)
 
   while True:
     x_new = g(x_list[-1])
@@ -17,10 +14,8 @@ def fixed_point(g, x0, tol=0.001):
         f" {iteration:9d} | {x_list[-2]:^9.5f} | {x_new:^9.5f} | {difference:.5f}")
 
     if difference < tol:
+      print("-" * 50)
       print("Approximated root: ", x_new)
       break
 
     iteration += 1
-
-
-fixed_point(g, 1.5)
--- open_methods/newton_rhapson.py
+++ open_methods/newton_rhapson.py
@@ -1,16 +1,9 @@
-def f(x):
-  return 2*x**3 - 2*x - 5
-
-
-def f_prime(x):
-  return 6*x**2 - 2
-
-
 def newton_raphson(f, f_prime, x0, tol=0.0001):
   x_list = [x0]
   iteration = 1
 
   print(f" Iteration | {'x_n':^9s} | {'x_n+1':^9s} | {'Difference'}")
+  print("-" * 50)
 
   while True:
     x_new = x_list[-1] - f(x_list[-1]) / f_prime(x_list[-1])
@@ -21,10 +14,8 @@ def newton_raphson(f, f_prime, x0, tol=0.0001):
         f" {iteration:9d} | {x_list[-2]:^9.5f} | {x_new:^9.5f} | {difference:.5f}")
 
     if difference < tol:
+      print("-" * 50)
       print("Approximated root: ", x_new)
       break
 
     iteration += 1
-
-
-newton_raphson(f, f_prime, 1.5)
