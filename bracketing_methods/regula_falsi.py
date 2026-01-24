7a4eaa156e29208a3ece8a165c88a76d5e75acaf
Aureeeus <johndavidlibrado@gmail.com>
2026-01-24 12:17:18 +0800

    Renamed variable names from mid -> c. More descriptive + file is now exlusively a utility.

--- bracketing_methods/regula_falsi.py
+++ bracketing_methods/regula_falsi.py
@@ -1,39 +1,31 @@
-def f(x):
-  return 2*x**3 - 2*x - 5
-
-
-def regula_falsi(f, a, b, tol):
+def regula_falsi(f, a, b, tol=0.001):
   if f(a) * f(b) >= 0:
     raise ValueError("f(a) and f(b) must have different signs.")
 
   iteration = 1
-  prev_mid = None
+  prev_c = None
 
   print(
-      f" Iteration | {'a':^9s} | {'b':^9s} | {'mid':^9s} | {'f(mid)':^9s} | abs error")
+      f" Iteration | {'a':^9s} | {'b':^9s} | {'c':^9s} | {'f(c)':^9s} | abs error")
   print("-" * 70)
 
   while True:
-    mid = (a * f(b) - b * f(a)) / (f(b) - f(a))
-    f_mid = f(mid)
-    error = abs(mid - prev_mid) if prev_mid is not None else None
+    c = (a * f(b) - b * f(a)) / (f(b) - f(a))
+    f_c = f(c)
+    error = abs(c - prev_c) if prev_c is not None else None
 
     print(
-        f" {iteration:^9d} | {a:^9.5f} | {b:^9.5f} | {mid:^9.5f} | {f_mid:^9.5f} | {"None" if error is None else f'{error:.5f}'}")
+        f" {iteration:^9d} | {a:^9.5f} | {b:^9.5f} | {c:^9.5f} | {f_c:^9.5f} | {"None" if error is None else f'{error:.5f}'}")
 
     if error is not None and error < tol:
       print("-" * 70)
-      print(f"Approximated root: ", mid)
+      print(f"Approximated root: ", c)
       break
 
-    if f(a) * f_mid < 0:
-      b = mid
+    if f(a) * f_c < 0:
+      b = c
     else:
-      a = mid
+      a = c
 
-    prev_mid = mid
+    prev_c = c
     iteration += 1
-
-
-if __name__ == "__main__":
-  regula_falsi(f, 1, 2, 0.001)
